50ad5bacdbb6b8dc98f59e521efa00cd328911b9
Yuting Li <yuting.li@mail.mcgill.ca>
2024-10-19 04:49:23 -0400

    Init

---
+++ src/main/java/fans/goldenglow/plumaspherebackend/PlumaSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package fans.goldenglow.plumaspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumaSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumaSphereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fans/goldenglow/plumaspherebackend/PlumaSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fans.goldenglow.plumaspherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumaSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
