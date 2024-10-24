a14cff2fe8ea4efe50a3841fe8b50d40ea8ae21b
Yuting Li <yuting.li@mail.mcgill.ca>
2024-10-24 11:56:57 -0400

    Move the code generating jwt token to an independent class

---
+++ src/main/java/fans/goldenglow/plumaspherebackend/util/JWTUtil.java
@@ -0,0 +1,28 @@
+package fans.goldenglow.plumaspherebackend.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import fans.goldenglow.plumaspherebackend.service.SystemConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@Service
+public class JWTUtil {
+    @Autowired
+    private SystemConfigService systemConfigService;
+
+    public HashMap<String, String> generateToken(String username) {
+        Date now = new Date();
+        Date expire = new Date(now.getTime() + 15 * 60 * 1000);
+        Date refresh_expire = new Date(now.getTime() + 30 * 60 * 1000);
+        HashMap<String, String> result = new HashMap<>();
+        String secretKey = systemConfigService.get("secret_key").orElse(systemConfigService.generateSecretKey());
+        result.put("token", JWT.create().withIssuer(username).withIssuedAt(now).withExpiresAt(expire).sign(Algorithm.HMAC256(secretKey)));
+        result.put("refresh_token", JWT.create().withIssuer(username).withIssuedAt(now).withExpiresAt(refresh_expire).sign(Algorithm.HMAC256(secretKey)));
+        return result;
+    }
+}
