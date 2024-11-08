e733f4a9693191d572e651286d40f30d10f7ba3e
Yuting Li <yuting.li@mail.mcgill.ca>
2024-11-07 23:58:29 -0500

    Format the code

--- src/main/java/fans/goldenglow/plumaspherebackend/entity/Like.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/entity/Like.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
--- src/main/java/fans/goldenglow/plumaspherebackend/entity/Post.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/entity/Post.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
