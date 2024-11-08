66c0ae4d7005cce8880575f6bb896716047a6d20
Yuting Li <yuting.li@mail.mcgill.ca>
2024-11-08 01:32:30 -0500

    Clear code

--- src/main/java/fans/goldenglow/plumaspherebackend/dto/CategoryDto.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/dto/CategoryDto.java
@@ -1,13 +1,8 @@
 package fans.goldenglow.plumaspherebackend.dto;
 
-import fans.goldenglow.plumaspherebackend.entity.Post;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
