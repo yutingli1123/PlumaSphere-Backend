79f91041e082e85963b33e8dbb1d7cc676d033df
Yuting Li <yutingli1123@outlook.com>
2025-04-27 13:00:56 +0800

    Add system initialization endpoints and supporting InitDto for setup configuration.

--- src/main/java/fans/goldenglow/plumaspherebackend/controller/SystemController.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/controller/SystemController.java
@@ -1,26 +1,40 @@
 package fans.goldenglow.plumaspherebackend.controller;
 
+import fans.goldenglow.plumaspherebackend.constant.ConfigField;
+import fans.goldenglow.plumaspherebackend.constant.UserRoles;
+import fans.goldenglow.plumaspherebackend.dto.InitDto;
 import fans.goldenglow.plumaspherebackend.entity.Config;
+import fans.goldenglow.plumaspherebackend.entity.User;
 import fans.goldenglow.plumaspherebackend.service.ConfigService;
+import fans.goldenglow.plumaspherebackend.service.PasswordService;
+import fans.goldenglow.plumaspherebackend.service.RedisService;
+import fans.goldenglow.plumaspherebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+
+import static fans.goldenglow.plumaspherebackend.constant.RedisKey.INITIALIZATION_CODE_KEY;
 
 @RestController
 @RequestMapping("/api/v1")
 public class SystemController {
     private final ConfigService configService;
+    private final RedisService redisService;
+    private final PasswordService passwordService;
+    private final UserService userService;
+
 
     @Autowired
-    public SystemController(ConfigService configService) {
+    public SystemController(ConfigService configService, RedisService redisService, PasswordService passwordService, UserService userService) {
         this.configService = configService;
+        this.redisService = redisService;
+        this.passwordService = passwordService;
+        this.userService = userService;
     }
 
     @GetMapping("/status")
@@ -35,8 +49,39 @@ public class SystemController {
     }
 
     @PostMapping("/init")
-    public ResponseEntity<Boolean> initSystem() {
-        // TODO
-        return ResponseEntity.internalServerError().build();
+    public ResponseEntity<Void> initSystem(@RequestBody InitDto initDto) {
+        try {
+            String verificationCode = initDto.getVerificationCode();
+            boolean verified = verify(verificationCode);
+            if (!verified) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+            User user = new User(initDto.adminUsername, passwordService.encodePassword(initDto.adminPassword));
+            user.setRole(UserRoles.ADMIN);
+            userService.save(user);
+            configService.set(ConfigField.BLOG_TITLE, initDto.getBlogTitle());
+            configService.set(ConfigField.BLOG_SUBTITLE, initDto.getBlogSubtitle());
+
+            configService.set(ConfigField.INITIALIZED, "true", true);
+            redisService.delete(INITIALIZATION_CODE_KEY);
+
+            return ResponseEntity.ok().build();
+        } catch (IllegalStateException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/init/verify-code")
+    public ResponseEntity<Boolean> verifyCode(@RequestBody String verifyCode) {
+        try {
+            return ResponseEntity.ok(verify(verifyCode));
+        } catch (IllegalStateException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    private boolean verify(String verificationCode) throws IllegalStateException {
+        String redisVerificationCode = redisService.get(INITIALIZATION_CODE_KEY);
+        if (redisVerificationCode == null) throw new IllegalStateException("Initialization code is not set yet.");
+        return redisVerificationCode.equals(verificationCode);
     }
 }
