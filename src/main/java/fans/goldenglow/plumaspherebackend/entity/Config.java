87dcc8f33840212b0b92f3358d8d2ee073836311
Yuting Li <yutingli1123@outlook.com>
2025-04-27 10:45:22 +0800

    Add @JsonIgnore annotation to isOpenToPublic field in Config entity.

--- src/main/java/fans/goldenglow/plumaspherebackend/entity/Config.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/entity/Config.java
@@ -1,5 +1,6 @@
 package fans.goldenglow.plumaspherebackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
@@ -27,6 +28,7 @@ public class Config implements Serializable {
     @NotEmpty
     private String configValue;
 
+    @JsonIgnore
     private Boolean isOpenToPublic = false;
 
     public Config(String configKey, String configValue) {
