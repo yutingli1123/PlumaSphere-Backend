c4e28f628f2365ea1a045106fd69f1f81e97fa37
Yuting Li <yutingli1123@outlook.com>
2025-04-27 13:00:46 +0800

    Add RedisService for key-value operations and RedisKey constant for initialization code key.

---
+++ src/main/java/fans/goldenglow/plumaspherebackend/constant/RedisKey.java
@@ -0,0 +1,5 @@
+package fans.goldenglow.plumaspherebackend.constant;
+
+public class RedisKey {
+    public static final String INITIALIZATION_CODE_KEY = "initialization:code";
+}
---
+++ src/main/java/fans/goldenglow/plumaspherebackend/service/RedisService.java
@@ -0,0 +1,27 @@
+package fans.goldenglow.plumaspherebackend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RedisService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void set(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public boolean delete(String key) {
+        return redisTemplate.delete(key);
+    }
+}
