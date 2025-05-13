599384ec760f94095c5efcd0c612a89e1d00036f
Yuting Li <yutingli1123@outlook.com>
2025-05-13 22:39:59 +0800

    Add WebSocket support with WebSocketConfig and WebSocketHandler

---
+++ src/main/java/fans/goldenglow/plumaspherebackend/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package fans.goldenglow.plumaspherebackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final CorsProperties corsProperties;
+    private final WebSocketHandler webSocketHandler;
+
+    @Autowired
+    public WebSocketConfig(CorsProperties corsProperties, WebSocketHandler webSocketHandler) {
+        this.corsProperties = corsProperties;
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins(String.valueOf(corsProperties.getAllowedOrigins()));
+    }
+}
---
+++ src/main/java/fans/goldenglow/plumaspherebackend/handler/WebSocketHandler.java
@@ -0,0 +1,71 @@
+package fans.goldenglow.plumaspherebackend.handler;
+
+import io.micrometer.common.lang.NonNullApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Slf4j
+@Component
+@NonNullApi
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final Map<Long, CopyOnWriteArraySet<WebSocketSession>> webSocketSessionMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        Optional<Long> postIdOption = getPostIdFromSession(session);
+        postIdOption.ifPresent(postId -> webSocketSessionMap
+                .computeIfAbsent(postId, key -> new CopyOnWriteArraySet<>())
+                .add(session));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        Optional<Long> postIdOption = getPostIdFromSession(session);
+        if (postIdOption.isPresent()) {
+            Long postId = postIdOption.get();
+            CopyOnWriteArraySet<WebSocketSession> webSocketSessions = webSocketSessionMap.get(postId);
+            if (webSocketSessions != null) {
+                webSocketSessions.remove(session);
+                if (webSocketSessions.isEmpty()) {
+                    webSocketSessionMap.remove(postId);
+                }
+            }
+        }
+    }
+
+    public void sendMessageToPost(Long postId, String message) {
+        CopyOnWriteArraySet<WebSocketSession> webSocketSessions = webSocketSessionMap.get(postId);
+        if (webSocketSessions != null) {
+            for (WebSocketSession session : webSocketSessions) {
+                if (session.isOpen()) {
+                    try {
+                        session.sendMessage(new TextMessage(message));
+                    } catch (IOException e) {
+                        log.warn("WebSocket: ", e);
+                    }
+                }
+            }
+        }
+    }
+
+    private Optional<Long> getPostIdFromSession(WebSocketSession session) {
+        URI uri = session.getUri();
+        if (uri == null) return Optional.empty();
+        String query = uri.getQuery();
+        if (query != null && query.contains("postId=")) {
+            return Optional.of(Long.valueOf(query.split("=")[1]));
+        }
+        return Optional.empty();
+    }
+}
