eb374817b412f60a5195dfacfd173adf173c3dda
Yuting Li <yutingli1123@outlook.com>
2025-05-14 00:35:10 +0800

    Implement message handling in WebSocketHandler to close session on message receipt

--- src/main/java/fans/goldenglow/plumaspherebackend/handler/WebSocketHandler.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/handler/WebSocketHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -43,6 +44,11 @@ public class WebSocketHandler extends TextWebSocketHandler {
                 }
             }
         }
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        session.close();
     }
 
     public void sendMessageToPost(Long postId, WebSocketMessageType message) {
