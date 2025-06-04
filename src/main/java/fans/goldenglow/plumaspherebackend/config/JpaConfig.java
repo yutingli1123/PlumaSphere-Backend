eb358f8dadb3d15bda7fd3374ab3a31c3ba38932
Yuting Li <yutingli1123@outlook.com>
2025-06-04 21:26:56 +0800

    Remove duplicate JpaAuditing config.

--- src/main/java/fans/goldenglow/plumaspherebackend/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package fans.goldenglow.plumaspherebackend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
