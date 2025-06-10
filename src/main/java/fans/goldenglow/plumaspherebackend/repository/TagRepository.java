05f8df69356651ef2065a1c2edcf90c9b5525f35
Yuting Li <yutingli1123@outlook.com>
2025-06-10 21:55:40 +0800

    Refactor TagRepository and TagService to use Optional for findByName method

--- src/main/java/fans/goldenglow/plumaspherebackend/repository/TagRepository.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/repository/TagRepository.java
@@ -4,7 +4,9 @@ import fans.goldenglow.plumaspherebackend.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByName(String name);
+    Optional<Tag> findByName(String name);
 }
--- src/main/java/fans/goldenglow/plumaspherebackend/service/TagService.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/service/TagService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -32,11 +33,8 @@ public class TagService {
         if (tags == null || tags.isEmpty()) return Set.of();
 
         return tags.stream().map(tagName -> {
-            Tag tag = tagRepository.findByName(tagName);
-            if (tag == null) {
-                tag = new Tag(tagName);
-            }
-            return tag;
+            Optional<Tag> tag = tagRepository.findByName(tagName);
+            return tag.orElseGet(() -> new Tag(tagName));
         }).collect(Collectors.toSet());
     }
 }
