b1b81517819c9906acb6a9b9f3ef5e31e5441d87
Yuting Li <yutingli1123@outlook.com>
2025-06-22 21:57:14 +0800

    Add JpaAuditingConfig to enable JPA auditing and remove redundant annotation from PlumaSphereBackendApplication

--- src/main/java/fans/goldenglow/plumaspherebackend/PlumaSphereBackendApplication.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/PlumaSphereBackendApplication.java
@@ -3,12 +3,10 @@ package fans.goldenglow.plumaspherebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
 @EnableAspectJAutoProxy
 public class PlumaSphereBackendApplication {
 
---
+++ src/main/java/fans/goldenglow/plumaspherebackend/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package fans.goldenglow.plumaspherebackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
