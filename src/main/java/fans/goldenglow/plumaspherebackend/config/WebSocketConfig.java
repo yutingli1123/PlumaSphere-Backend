6eb0f8acebc3f18844e02036d12ff2b8d20d6a5d
Yuting Li <yutingli1123@outlook.com>
2025-06-23 22:04:04 +0800

    Add @NonNull annotation to resource handler parameters in WebMvcConfig and WebSocketConfig

--- src/main/java/fans/goldenglow/plumaspherebackend/config/WebSocketConfig.java
+++ src/main/java/fans/goldenglow/plumaspherebackend/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package fans.goldenglow.plumaspherebackend.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -15,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final WebSocketHandler webSocketHandler;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/ws")
                 .setAllowedOrigins(corsProperties.getAllowedOrigins().toArray(String[]::new));
     }
